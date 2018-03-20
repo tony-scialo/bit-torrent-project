56380bcd2e070dd3d5f412c536ecc5c84e8cef33
thegodfather904 <arscialo@gmail.com>
2018-03-19 23:00:40 -0400

    added peer id as command line arg

--- src/peerProcess.java
+++ src/peerProcess.java
@@ -3,6 +3,14 @@ import java.util.ArrayList;
 
 public class peerProcess {
     public static void main(String[] args) {
+
+        if(args.length == 0){
+            System.out.println("Error: peer id not provided, closing program");
+            return;
+        }
+
+        int peerId = Integer.parseInt(args[0]);
+
         // parse the 2 files Common.cfg and PeerInfo.cfg
         FileParser parser = new FileParser();
         try{
