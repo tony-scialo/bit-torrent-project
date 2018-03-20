43c3a89037a674a1010d5be4ba94d93bcc91930a
thegodfather904 <arscialo@gmail.com>
2018-03-19 22:17:33 -0400

    peer info being parsed correctly

--- src/FileParser.java
+++ src/FileParser.java
@@ -1,5 +1,8 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 class FileParser {
 
@@ -9,13 +12,39 @@ class FileParser {
 
     }
 
-    public PeerInfo parsePeerInfo() throws Exception {
-        PeerInfo pi = new PeerInfo();
-
+    public List<PeerInfo> parsePeerInfo() throws Exception {
+        List<PeerInfo> piList;
         String peerInfoRaw = convertFileToString(PEER_INFO);
-        System.out.println(peerInfoRaw);
+        piList = convertRawToPeerInfo(peerInfoRaw);
+        return piList;
+    }
 
-        return pi;
+    private List<PeerInfo> convertRawToPeerInfo(String peerInfoRaw){
+        List<PeerInfo> piList = new ArrayList<>();
+        Scanner scan = new Scanner(peerInfoRaw);
+        String peerInfoOneLine = "";
+        // not sure if one line or multiple so combine into one line
+        while(scan.hasNextLine()){
+            peerInfoOneLine += scan.nextLine() + " ";
+        }
+
+        String[] splitArray = peerInfoOneLine.split(" ");
+        PeerInfo pi;
+
+        for(int x = 0; x < splitArray.length; x += 4){
+            pi = new PeerInfo();
+            pi.setPeerId(Integer.parseInt(splitArray[x]));
+            pi.setHostName(splitArray[x + 1]);
+            pi.setPort(Integer.parseInt(splitArray[x + 2]));
+            pi.setFile((Integer.parseInt(splitArray[x + 3]) == 1));
+            piList.add(pi);
+        }
+
+        for (PeerInfo item : piList) {
+            System.out.println(item.toString());
+        }
+
+        return piList;
     }
 
     private String convertFileToString(String filename) throws Exception {
--- src/PeerInfo.model.java
+++ src/PeerInfo.model.java
@@ -1,25 +1,30 @@
 class PeerInfo {
-    private String peerId;
+    private int peerId;
     private String hostName;
     private int port;
     private boolean file;
+    private String rawString;
 
     public PeerInfo() {
 
     }
 
-    public PeerInfo(String peerId, String hostName, int port, boolean file) {
+    public PeerInfo(int peerId, String hostName, int port, boolean file) {
         this.peerId = peerId;
         this.hostName = hostName;
         this.port = port;
         this.file = file;
     }
 
-    public String getPeerId(){
+    public String toString(){
+        return peerId + " " + hostName + " " + port + " " + file;
+    }
+
+    public int getPeerId(){
         return peerId;
     }
 
-    public void setPeerId(String peerId){
+    public void setPeerId(int peerId){
         this.peerId = peerId;
     }
 
@@ -45,5 +50,13 @@ class PeerInfo {
 
     public void setFile(boolean file){
         this.file = file;
+    }
+
+    public String getRawString(){
+        return rawString;
+    }
+
+    public void setRawString(String rawString){
+        this.rawString = rawString;
     }
 }
